801dc034520446172038292bc5ac0d35c79b8a4b
glloret <4tecno@gmail.com>
2013-07-15 18:39:21 +0200

    Estilo CSS básico, con imágenes y nombre de usuario

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var client = require("twitter-api").createClient();
+
+// En este archivo están guardadas las claves de autentificación en al API de Twitter
 var clave =require('./claves.js').clave;
-//console.log(clave);
 
 client.setAuth ( 
     clave.consumer,
@@ -10,12 +11,8 @@ client.setAuth (
     clave.requestsecret 
 );
 
-
-
 var app = express();
 var port = 3700;
-
-
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -29,19 +26,32 @@ var io = require('socket.io').listen(app.listen(port));
 
 console.log("Listening on port " + port);
 
-var num = 0,
-    max = 10;
+// var num = 0,
+//     max = 10;
 
-client.stream( 'statuses/filter', { track: '#bodatest' }, function( json ){
+client.stream( 'statuses/filter', { track: '#secrettest' }, function( json ){
     var tweet = JSON.parse( json );
     if( tweet.text && tweet.user ){
-        console.log( tweet.user.screen_name+': "'+tweet.text+'" ' + tweet.user.profile_image_url );
-        io.sockets.emit('message', { mensaje: tweet.text, profile_image_url:tweet.user.profile_image_url  });
+        //console.log( tweet.user.screen_name+': "'+tweet.text+'" ' + tweet.user.profile_image_url );
+        
+        // Así se accede a las imagenes del post
 
-        if( ++num === max ){
-            console.log('----');
-            client.abort();
+        //console.log (tweet.entities.media[0].media_url);
+        if (tweet.entities.media){
+            console.log("Tweet con imagen");
+            io.sockets.emit('message', { nombre: tweet.user.screen_name, mensaje: tweet.text, profile_image_url:tweet.user.profile_image_url, media_image:tweet.entities.media[0].media_url  });
+
+        } else {
+            console.log("Tweet sin imagen");
+            io.sockets.emit('message', { nombre: tweet.user.screen_name, mensaje: tweet.text, profile_image_url:tweet.user.profile_image_url  });
         }
+        //console.log (tweet);
+        //io.sockets.emit('message', { mensaje: tweet.text, profile_image_url:tweet.user.profile_image_url  });
+
+        // if( ++num === max ){
+        //     console.log('----');
+        //     client.abort();
+        //}
     }
 } );
 
