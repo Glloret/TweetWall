41cb5ea3ad1506ae9f4194474279707c5f586344
glloret <4tecno@gmail.com>
2013-08-31 16:34:03 +0200

    alpha2

---
+++ public/tweet.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+ 
+    var messages = [];
+    //var socket = io.connect('http://192.168.1.11:3700');
+    var socket = io.connect('127.0.0.1:3700');
+    
+    var content = $('#content');
+    var scroll=500;
+    socket.on('message', function (data) {
+        if(data.media_image) {
+
+            html='<div class="tweet"> <div class="avatar"> <div class="avatar_img"> <img src="'+ data.profile_image_url +'"/> </div> <div class="nombre">'+ data.nombre +'</div> </div> <div class="texto">' + data.mensaje + '<img src="'+ data.media_image +':small "/> </div> </div>';
+            $('#content').append(html);
+            scroll = scroll + $('body>div>div.tweet:last-child').height();
+            $('#content').animate({scrollTop: scroll },2000 );
+            
+        } else if (data){
+            html='<div class="tweet"> <div class="avatar"> <div class="avatar_img"> <img src="'+ data.profile_image_url +'"/> </div> <div class="nombre">'+ data.nombre +'</div> </div> <div class="texto">'+ data.mensaje + '</div> </div>';
+            $('#content').append(html);
+            scroll = scroll + $('body>div>div.tweet:last-child').height();
+            $('#content').animate({scrollTop: scroll },2000 );
+            
+        } else {
+            console.log("There is a problem:", data);
+
+        }
+    });
+ 
+    
+ 
+}
